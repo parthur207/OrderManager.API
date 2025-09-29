df62342c07e1bf7019c81c788e7fc229d2f9313b
Paulo Andrade <parthur207@gmail.com>
2025-09-29 18:51:09 -0300

    Implementação da funcionalidade de verificação de tempo de incremento de ocorrencias em um pedido.

--- OrderManager.Infrastructure/Repository/Admin/CheckTimeOccurenceOrderRepository.cs
+++ OrderManager.Infrastructure/Repository/Admin/CheckTimeOccurenceOrderRepository.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.Application.RepositoryInterface;
+using OrderManager.Domain.Enuns;
+using OrderManager.Domain.Models.ReponsePattern;
 using OrderManager.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -9,9 +13,40 @@ using System.Threading.Tasks;
 
 namespace OrderManager.Infrastructure.Repository.Admin
 {
-    public class CheckTimeOccurenceOrderRepository
+    public class CheckTimeOccurenceOrderRepository : ICheckTimeOccurrenceOrderInterfaceRespository
     {
 
-        private readonly DbContextInMemory
+        private readonly DbContextOrderManager _dbContextOM;
+
+        public async Task<SimpleResponseModel> CheckTimeRepository(int orderNumber, ETypeOccurrenceEnum TypeOccurrence)
+        {
+            SimpleResponseModel Response = new SimpleResponseModel();
+
+            try
+            {
+                var order = await _dbContextOM.OrderEntity.FirstOrDefaultAsync(x=>x.OrderNumber==orderNumber);
+
+                if (order is null)
+                {
+                    Response.Message = $"Não foi encontrado nenhum pedido com o número informado: {orderNumber}";
+                    Response.Status = ResponseStatusEnum.NotFound;
+                    return Response;
+                }
+
+                if (order.Occurrences.Any(x=>x.TypeOccurrence.Equals(TypeOccurrence)) && order.UpdatedAt)
+                {
+
+                }
+
+                Response.Status= ResponseStatusEnum.Success;
+            }
+            catch (Exception ex)
+            {
+                Response.Status= ResponseStatusEnum.Error;
+                Response.Message= "Erro ao verificar o tempo de ocorrência do pedido: "+ ex.Message;
+                Debug.Assert(false, Response.Message);
+            }
+            return Response;
+        }
     }
 }
