f1ea53c630556d0c766ba2afeb15f42e34e64263
Paulo Andrade <parthur207@gmail.com>
2025-09-30 21:12:44 -0300

    Criação e implementação de serviço de login, que retorna o ID e a Role do usuário.

---
+++ OrderManager.Application/RepositoryInterface/Login/IUserLoginDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using OrderManager.Domain.Entities;
+using OrderManager.Domain.Enuns;
+using OrderManager.Domain.Models.ReponsePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager.Application.RepositoryInterface.Login
+{
+    public interface IUserLoginDataRepository
+    {
+        Task<ResponseModel<(int, RoleEnum)>?> LoginRepository(UserEntity userDataEntity);//Retorna o ID do usuario e sua 'role'
+    }
+}
---
+++ OrderManager.Infrastructure/Repository/Login/UserLoginDataRepository.cs
@@ -0,0 +1,53 @@
+﻿using OrderManager.Application.RepositoryInterface.Login;
+using OrderManager.Domain.Entities;
+using OrderManager.Domain.Enuns;
+using OrderManager.Domain.Models.ReponsePattern;
+using OrderManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager.Infrastructure.Repository.Login
+{
+    public class UserLoginDataRepository : IUserLoginDataRepository
+    {
+        private readonly DbContextOrderManager _dbContextOM;
+
+        public UserLoginDataRepository(DbContextOrderManager dbContextOM)
+        {
+            _dbContextOM = dbContextOM;
+        }
+        public async Task<ResponseModel<(int, RoleEnum)>?> LoginRepository(UserEntity userDatalogin)
+        {
+            ResponseModel<(int, RoleEnum)> Response = new ResponseModel<(int, RoleEnum)>();
+
+            try
+            {
+                var UserExists = await _dbContextOM.UserEntity
+                     .Where(x => x.IsActive == true)
+                     .FirstOrDefaultAsync(x => x.Email == userDatalogin.Email && x.Password==userDatalogin.Password);
+
+                if (UserExists is null)
+                {
+                    Response.Message = "Email ou senha incorretos.";
+                    Response.Status=ResponseStatusEnum.NotFound;
+                    return Response;
+                }
+
+                Response.Status=ResponseStatusEnum.Success;
+                Response.Content=(UserExists.Id, UserExists.Role);
+            }
+            catch (Exception ex)
+            {
+                Response.Status=ResponseStatusEnum.CriticalError;
+                Response.Message="Ocorreu um erro inesperado: " + ex.Message;
+                Debug.Assert(false, Response.Message);
+            }
+            return Response;
+        }
+    }
+}
