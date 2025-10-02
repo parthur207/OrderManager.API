2a429613f6709603ad9ae7f4942c8740128c7a20
Paulo Andrade <parthur207@gmail.com>
2025-10-01 21:10:18 -0300

    Estruturação do mapeador de entidade/Dto de usuário.

--- OrderManager.Application/Mappers/UserMapper.cs
+++ OrderManager.Application/Mappers/UserMapper.cs
@@ -1,10 +1,13 @@
 ﻿using OrderManager.Application.DTOs.Adm;
 using OrderManager.Application.Mappers.MappersInterface;
 using OrderManager.Domain.Entities;
+using OrderManager.Domain.Enuns;
 using OrderManager.Domain.Models;
 using OrderManager.Domain.Models.ReponsePattern;
+using OrderManager.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,19 +16,107 @@ namespace OrderManager.Application.Mappers
 {
     public class UserMapper : IUserMapperInterface
     {
-        public ResponseModel<UserEntity>? UserCreateModelToEntity(CreateUserModel UserModel)
+        private readonly IOrderMapperInterface _orderMapperInterface;
+        public UserMapper(IOrderMapperInterface orderMapperInterface)
         {
-            throw new NotImplementedException();
+            _orderMapperInterface = orderMapperInterface;
         }
 
-        public ResponseModel<UserAdmDTO>? UserEntityToDTO(UserEntity entity)
+        public ResponseModel<UserEntity>? UserCreateModelToEntity(CreateUserModel UserModel)
         {
-            throw new NotImplementedException();
+            ResponseModel<UserEntity>? Response= new ResponseModel<UserEntity>();
+            try
+            {
+                if (UserModel is null)
+                {
+                    Response.Message ="Erro. O modelo de usuário não pode ser nulo.";
+                    Response.Status = ResponseStatusEnum.Error;
+                    return Response;
+                }
+
+                var UserEntityConverted = new UserEntity
+                (
+                    UserModel.Name,
+                    new UserEmailVO(UserModel.Email),
+                    new PasswordVO(UserModel.Password),
+                    UserModel.Address
+                );
+
+                Response.Content = UserEntityConverted;
+                Response.Status = ResponseStatusEnum.Success;
+            }
+            catch (Exception EX)
+            {
+                Response.Status = ResponseStatusEnum.CriticalError;
+                Response.Message = $"Ocorreu um erro inesperado: "+EX.Message;
+                Debug.Assert(false, Response.Message);
+            }
+            return Response;
         }
 
         public ResponseModel<UserEntity>? UserLoginModelToEntity(UserLoginModel UserModel)
         {
-            throw new NotImplementedException();
+            ResponseModel<UserEntity>? Response = new ResponseModel<UserEntity>();
+            try
+            {
+                if (UserModel is null)
+                {
+                    Response.Message = "Erro. O modelo de usuário para login não pode ser nulo.";
+                    Response.Status = ResponseStatusEnum.Error;
+                    return Response;
+                }
+
+                var UserEntityConverted = new UserEntity
+                (
+                    new UserEmailVO(UserModel.Email),
+                    new PasswordVO(UserModel.Password)
+                );
+
+                Response.Content = UserEntityConverted;
+                Response.Status = ResponseStatusEnum.Success;
+            }
+            catch (Exception EX)
+            {
+                Response.Status = ResponseStatusEnum.CriticalError;
+                Response.Message = $"Ocorreu um erro inesperado: " + EX.Message;
+                Debug.Assert(false, Response.Message);
+            }
+            return Response;
+        }
+
+        //entity=>dto
+        public ResponseModel<UserAdmDTO>? UserEntityToDTO(UserEntity entity)
+        {
+            ResponseModel<UserAdmDTO>? Response = new ResponseModel<UserAdmDTO>();
+            try
+            {
+                if (entity is null)
+                {
+                    Response.Message = "Erro. A entidade de usuário não pode ser nula.";
+                    Response.Status = ResponseStatusEnum.Error;
+                    return Response;
+                }
+
+                var userDTOConverted = new UserAdmDTO
+                (
+                    entity.Name,
+                    entity.Email.Value,
+                    entity.Address,
+                    _orderMapperInterface.OrderEntityListToDTOList(entity.OrderList).Content,
+                    entity.CreatedAt,
+                    entity.Role
+                );
+
+                Response.Content = userDTOConverted;
+                Response.Status = ResponseStatusEnum.Success;
+            }
+            catch (Exception EX)
+            {
+                Response.Status = ResponseStatusEnum.CriticalError;
+                Response.Message = $"Ocorreu um erro inesperado: " + EX.Message;
+                Debug.Assert(false, Response.Message);
+            }
+            return Response;
         }
     }
 }
